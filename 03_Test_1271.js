db3c14639aee049798717790762c166125d2fda4
yejz0715 <yejz0715@gmail.com>
2022-03-22 22:15:22 +0900

    add 1271

---
+++ 03_Test_1271.js
@@ -0,0 +1,28 @@
+const readline = require("readline");
+
+//엄청난 부자:n(돈),m(생명체), answer1(한명당 나눠줄수있는돈),answer2(나머지)
+
+function solution(input) { 
+ 
+  let result=input; 
+   let numArr = result.split(" "); 
+   
+   let n=BigInt(numArr[0]);  
+   let m=BigInt(numArr[1]);
+  //Number->BigInt :입력받는 수가 상당히 크다는 것을 인지하고 Number 값이 안정적으로 나타낼 수 있는 최대치보다 더 큰 정수를 표현할 수 있는 내장 객체
+  
+   const answer1=n/m; 
+   const answer2=n%m;
+  
+   console.log(`${answer1}`);
+   console.log(`${answer2}`);
+}
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.on("line", (line) => {
+  console.log(solution(line));
+});
