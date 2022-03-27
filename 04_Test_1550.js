d8399109e0fcf43ad2e8f664e5c6765677dd29e9
yejz0715 <yejz0715@gmail.com>
2022-03-27 19:40:13 +0900

    parseInt

--- 04_Test_1550.js
+++ 04_Test_1550.js
@@ -3,12 +3,18 @@ const readline = require("readline");
 
 
 function solution(input) { 
- 
-   let result=parseInt(input,16);
+ var arr=[0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F',];
+arr['A']=10;
+arr['B']=11;
+arr['C']=12;
+arr['D']=13;
+arr['E']=14;
+arr['F']=15;
+console.log(`${arr}`);
+   //let result=parseInt(input,16);
     
-    console.log(result);
    
-   
+   console.log(result);
 }
 
 const rl = readline.createInterface({
