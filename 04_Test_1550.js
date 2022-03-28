09f0ea4df7ddda29ca464e298769af656b997453
yejz0715 <yejz0715@gmail.com>
2022-03-28 22:54:27 +0900

    add comment

--- 04_Test_1550.js
+++ 04_Test_1550.js
@@ -1,9 +1,9 @@
 const readline = require("readline");
 
-
+////출력o, 백준출력x
 //16진수->10진수( 최대 길이는 6글자, 음수x)
 function solution(input) { 
-
+//let input={length:5};
   let result = input.split(" "); 
    let result2=parseInt(result,16);  
    //parseInt:문자열을 정수로 반환(문자열string,사용할진법16(16진수->10진수))
