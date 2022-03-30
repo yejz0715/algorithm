3ae8f9b4135db79e9b2c6793b4f195b337c53bbd
unknown <yejz0715@gmail.com>
2022-03-30 23:48:35 +0900

    Modify the output method

--- 05_Test_2338.js
+++ 05_Test_2338.js
@@ -1,21 +1,19 @@
 const readline = require("readline");
 
 //긴자리 계산 //첫째줄에 n, 둘째줄에 m 입력
-function solution(input) { 
-    let result=input; 
-    let numArr = result.split(' '); 
-    
-    let n=Number(numArr[0]);  
-    let m=Number(numArr[1]);
-   
-    const answer1=n+m;
-    const answer2=n-m; 
-    const answer3=n*m;
-   
-    console.log(`${answer1}`);
-    console.log(`${answer2}`);  
-    console.log(`${answer3}`); 
+function solution(input) {
+  let n = input[0];
+  let m = input[1];
+
+  const answer1 = n + m;
+  const answer2 = n - m;
+  const answer3 = n * m;
+
+  console.log(`${answer1}`);
+  console.log(`${answer2}`);
+  console.log(`${answer3}`);
 }
+let input = [];
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -23,5 +21,9 @@ const rl = readline.createInterface({
 });
 
 rl.on("line", (line) => {
-  console.log(solution(line));
+  //console.log(typeof line);
+  input.push(BigInt(line)); //Number->BigInt(범위때문)
+  if (input.length === 2) {
+    solution(input);
+  }
 });
