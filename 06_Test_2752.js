cb2e854c42d2efc2979788aa1b2f6d73a27e899f
unknown <yejz0715@gmail.com>
2022-03-30 23:51:15 +0900

    Fixing error

--- 06_Test_2752.js
+++ 06_Test_2752.js
@@ -1,18 +1,24 @@
 const readline = require("readline");
 
 //세수정렬 sort() : 배열 안의 원소를 정렬하는 함수(오름차순)
-function solution(input) { 
-    let result=input; 
-    let numArr = result.split(' '); 
-    
-    let a=Number(numArr[0]);  
-    let b=Number(numArr[1]);
-    let c=Number(numArr[2]);
-   
-    let arr=[a,b,c];
-         
-    console.log(`${arr.sort()}`);
-     
+function solution(input) {
+  let result = "";
+  let numArr = input.split(" ");
+  let answer = [];
+
+  // map
+  numArr.map((num) => answer.push(Number(num)));
+  answer.sort((a, b) => a - b);
+
+  answer.map((num) => (result += num + " "));
+  console.log(result);
+  // let a = Number(numArr[0]);
+  // let b = Number(numArr[1]);
+  // let c = Number(numArr[2]);
+
+  // let arr = [a, b, c];
+
+  //console.log(`${arr.sort()}`);
 }
 
 const rl = readline.createInterface({
@@ -21,5 +27,5 @@ const rl = readline.createInterface({
 });
 
 rl.on("line", (line) => {
-  console.log(solution(line));
+  solution(line);
 });
