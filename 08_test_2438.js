f8a53a54b337b2a2204f5deaa6a13bd4160228a3
yejz0715 <yejz0715@gmail.com>
2022-04-08 03:25:48 +0900

    for문2개사용해서 별출력하기

--- 08_test_2438.js
+++ 08_test_2438.js
@@ -1,8 +1,16 @@
+//입력한 숫자 별찍기
 const readline = require("readline");
 function solution(input) {
-  for (i = 1; i <= input; i++) {
-    console.log("*");
+  let answer = ""; //String형 변수 생성
+  for (let i = 1; i <= input; i++) {
+    //i:행
+    for (let j = 1; j <= i; j++) {
+      //j:별 갯수
+      answer += "*"; //answer=answer+"*"
+    }
+    answer += "\n";
   }
+  return answer;
 }
 const rl = readline.createInterface({
   input: process.stdin,
@@ -10,5 +18,5 @@ const rl = readline.createInterface({
 });
 
 rl.on("line", (line) => {
-  solution(line);
+  console.log(solution(line));
 });
