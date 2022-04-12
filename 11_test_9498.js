c2a198dca1102ea073fa4db2d611fd05c9257893
yejz0715 <yejz0715@gmail.com>
2022-04-12 23:27:42 +0900

    if문

---
+++ 11_test_9498.js
@@ -0,0 +1,11 @@
+//시험성적
+const readline = require("readline");
+function solution() {}
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.on("line", (line) => {
+  solution(line);
+});
