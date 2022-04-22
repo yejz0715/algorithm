31880e00bec0b883e25862020ef9b1e52149a777
unknown <yejz0715@gmail.com>
2022-04-22 13:51:22 +0900

    백준알고리즘- 동혁 ing

--- 14_test_2720.js
+++ 14_test_2720.js
@@ -1,21 +1,25 @@
 // 세탁소 사장 동혁 거스름돈 계산
 const readline = require("readline");
 function solution(input) {
-  const quater = 25;
-  const dime = 10;
-  const nick = 5;
-  const penny = 1;
-
-  //const i = input[i];
-  for (i = 1; i <= input.length; i++) {
-    const a = input[i] / quater;
-    const b = a / dime;
-    const c = b / nick;
-    const d = c / penny;
-    console.log(`${a} ${b} ${c} ${d}`);
+  let numArr = input;
+  numArr.shift();
+  let answer = [];
+  let qdnp = [25, 10, 5, 1];
+  for (i = 1; i <= numArr.length; i++) {
+    for (j = 1; j <= qdnp.length; j++) {
+      if (numArr[i] >= qdnp[j]) {
+        answer += numArr[i] / qdnp[j];
+        //let remain = numArr[i] % qdnp[j];
+      } else if (numArr[i] < qdnp[j]) {
+        j++;
+      }
+    }
+    return answer;
   }
 }
+
 let arr = [];
+
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
@@ -23,5 +27,5 @@ const rl = readline.createInterface({
 
 rl.on("line", (line) => {
   arr.push(line);
-  if (arr.length - 1 == arr[0]) solution(arr);
+  if (arr.length - 1 == arr[0]) console.log(solution(arr));
 });
