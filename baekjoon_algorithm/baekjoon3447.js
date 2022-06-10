5a69eb6d040fe56723e569f67cbb680a450f59f3
unknown <yejz0715@gmail.com>
2022-06-10 21:41:03 +0900

    baekjoon3447_버그왕_BUG 제외하고 출력. (while문 이용)

--- baekjoon_algorithm/baekjoon3447.js
+++ baekjoon_algorithm/baekjoon3447.js
@@ -5,9 +5,15 @@ const input = require("fs")
   .split("\n");
 console.log(solution(input));
 function solution(input) {
-  let answer = [];
+  let answer = ""; //마지막에 담아서 출력할 변수
+  let line = "";
   for (let i = 0; i < input.length; i++) {
-    answer += input[i].replaceAll("BUG", "") + "\n";
+    line = input[i];
+    while (line.includes("BUG")) {
+      //while문으로 BUG가 없을때까지 계속 돌림
+      line = line.replaceAll("BUG", "");
+    }
+    answer += line + "\n";
   }
 
   return answer;
