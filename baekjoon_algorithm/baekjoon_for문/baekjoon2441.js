d2ec783f7a881bdd6bcd5c30fed585e9e83a9603
unknown <yejz0715@gmail.com>
2022-07-26 17:39:40 +0900

    baekjoon2441_별찍기4_for문

--- baekjoon_algorithm/baekjoon_for문/baekjoon2441.js
+++ baekjoon_algorithm/baekjoon_for문/baekjoon2441.js
@@ -1,6 +1,20 @@
 // 백준 별찍기 4
 const readline = require("readline");
-function solution(input) {}
+function solution(input) {
+  let answer = "";
+
+  for (let i = 0; i < input; i++) {
+    for (let j = 0; j < i; j++) {
+      answer += " ";
+    }
+    for (let o = input; o > i; o--) {
+      answer += "*";
+    }
+
+    answer += "\n";
+  }
+  return answer;
+}
 
 const rl = readline.createInterface({
   input: process.stdin,
