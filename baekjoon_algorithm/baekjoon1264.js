7ce89851395b9e8a767114ace41f87400171903b
unknown <yejz0715@gmail.com>
2022-08-12 14:08:49 +0900

    baekjoon1264_모음개수_이중포문

--- baekjoon_algorithm/baekjoon1264.js
+++ baekjoon_algorithm/baekjoon1264.js
@@ -1,8 +1,31 @@
 // 모음의 개수
+/*
+How are you today?
+Quite well, thank you, how about yourself?
+I live at number twenty four.
+#
+*/
 const readline = require("readline");
 function solution(input) {
-  let message = input;
-  for (let i = 0; i < message.length; i++) {}
+  let sum = 0;
+  let message = input.map((item) => item.split(""));
+  for (let i = 0; i < message.length; i++) {
+    for (let j = 0; j < message[i].length; j++) {
+      if (message[i][j] === "A" || message[i][j] === "a") {
+        sum += 1;
+      } else if (message[i][j] === "E" || message[i][j] === "e") {
+        sum += 1;
+      } else if (message[i][j] === "I" || message[i][j] === "i") {
+        sum += 1;
+      } else if (message[i][j] === "O" || message[i][j] === "o") {
+        sum += 1;
+      } else if (message[i][j] === "U" || message[i][j] === "u") {
+        sum += 1;
+      }
+    }
+    console.log(sum);
+    sum = 0;
+  }
 }
 let arr = [];
 const rl = readline.createInterface({
@@ -13,6 +36,7 @@ rl.on("line", (line) => {
   arr.push(line);
   if (line === "#") {
     //한줄에 "#" 있다면 함수실행
-    console.log(solution(arr));
+    arr.pop();
+    return solution(arr);
   }
 });
