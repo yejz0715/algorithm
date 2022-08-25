eee613531977c6fcbe2159cf07b59cd800aa017b
unknown <yejz0715@gmail.com>
2022-08-25 15:10:32 +0900

    baekjoon10809_상수(문자열)_두수를 거꾸로 비교하기_reverse()

---
+++ baekjoon_algorithm/baekjoon10809.js
@@ -0,0 +1,14 @@
+//알파벳 찾기
+const readline = require("readline");
+function solution(input) {
+  for (let j = 0; j < input.length; j++) {}
+  console.log(a);
+}
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+rl.on("line", (line) => {
+  console.log(solution(line));
+});
