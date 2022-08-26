2cdd83b03bbcc6a5c9ba2c7fb5a4fd219348ada3
unknown <yejz0715@gmail.com>
2022-08-26 14:33:29 +0900

    baekjoon10809_알파벳찾기_아스키코드 사용

--- baekjoon_algorithm/baekjoon10809.js
+++ baekjoon_algorithm/baekjoon10809.js
@@ -1,8 +1,16 @@
-//알파벳 찾기
+//알파벳 찾기 아스키코드 사용
 const readline = require("readline");
 function solution(input) {
-  for (let j = 0; j < input.length; j++) {}
-  console.log(a);
+  let alphabet = Array(26).fill(-1); //배열 생성
+  for (let i = 0; i < input.length; i++) {
+    let index = input.charCodeAt(i) - 97; //input[i]의 아스키번호-97=알파벳 인덱스
+    alphabet[index]++;
+    if (alphabet[index] !== -1) {
+      alphabet[index] = input.indexOf(input[i]);
+    }
+  }
+
+  return alphabet.join(" ");
 }
 
 const rl = readline.createInterface({
