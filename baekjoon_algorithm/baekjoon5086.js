02a0a9817e43f8a7bdec3e670d84181b22e26cf2
unknown <yejz0715@gmail.com>
2022-09-02 17:17:58 +0900

    baekjoon5086_배수와 약수

--- baekjoon_algorithm/baekjoon5086.js
+++ baekjoon_algorithm/baekjoon5086.js
@@ -1,17 +1,19 @@
 //배수와 약수
 const readline = require("readline");
 const solution = (input) => {
+  let answer = [];
   for (let i = 0; i < input.length; i++) {
     let left = Number(input[i].split(" ")[0]);
     let right = Number(input[i].split(" ")[1]);
     if (right % left == 0) {
-      console.log("factor");
+      answer.push("factor");
     } else if (left % right == 0) {
-      console.log("multiple");
+      answer.push("multiple");
     } else {
-      console.log("neither");
+      answer.push("neither");
     }
   }
+  return answer.join("\n");
 };
 
 const rl = readline.createInterface({
@@ -24,6 +26,6 @@ rl.on("line", (line) => {
   arr.push(line);
   if (line == "0 0") {
     arr.pop();
-    return solution(arr);
+    console.log(solution(arr));
   }
 });
