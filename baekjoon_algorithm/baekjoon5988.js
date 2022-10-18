b4b5d74e5e64f6bc44b153bb3c2196738aeabc00
unknown <yejz0715@gmail.com>
2022-10-19 01:27:36 +0900

    baekjoon5988_홀수일까 짝수일까_for문(일의자리 찾기),filter()

--- baekjoon_algorithm/baekjoon5988.js
+++ baekjoon_algorithm/baekjoon5988.js
@@ -1,15 +1,25 @@
-//홀수일까 짝수일까(백준에서 틀림 -> 이유 찾는중)
+//홀수일까 짝수일까
+//K (1 <= K <= 10^60): 입력받은 숫자는 자료형으로 접근 불가능한 큰 숫자라 문자열을 이용하거나 마지막 일의자리로 확인할 수 있다.
 const readline = require("readline");
 function solution(input) {
   let answer = "";
+  let numArr = [];
   for (let i = 0; i < input.length; i++) {
-    le;
-    if (input[i] % 2 === 0) {
-      answer += "even" + "\n";
-    } else {
-      answer += "odd" + "\n";
+    for (let j = input[i].length - 1; j >= 0; j--) {
+      numArr.push(input[i][j]);
+      break; //입력받은 숫자의 마지막 일의자리만 필요하기때문에
     }
   }
+  numArr
+    .map((num) => Number(num))
+    .filter((num) => {
+      num % 2 != 0 ? (answer += "odd" + "\n") : (answer += "even" + "\n");
+    });
+
+  //   answer += "even" + "\n";
+  // } else {
+  //   answer += "odd" + "\n";
+  // }
 
   return answer;
 }
