2c5a6645f6b9d6aa6968540571bd61ad40efe40d
unknown <yejz0715@gmail.com>
2022-11-11 17:40:29 +0900

    baekjoon2711_오타맨 고창영_map()_splice()

--- baekjoon_algorithm/baekjoon2711.js
+++ baekjoon_algorithm/baekjoon2711.js
@@ -1,10 +1,13 @@
-//오타맨 고창영
+//오타맨 고창영_1개의 오타를 제거한 문자열 출력
 const readline = require("readline");
 function solution(input) {
-  for (let i = 0; i < input.length; i++) {
-    let a = input[i][0];
-    for (let j = 0; input[i].length; j++) {}
-  }
+  let answer = "";
+  input.map((item) => {
+    const num = Number(item.split(" ")[0]);
+    const stringArr = item.split(" ")[1].split("");
+    stringArr.splice(num - 1, 1); //splice(시작인덱스,제거할 요소 수)
+    answer += stringArr.join("") + "\n";
+  });
   return answer;
 }
 let arr = [];
