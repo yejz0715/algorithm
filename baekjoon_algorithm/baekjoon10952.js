42b58d878d5cdd75022bf09a9601acd700c04f38
unknown <yejz0715@gmail.com>
2022-11-22 21:16:29 +0900

    baekjoon10952

---
+++ baekjoon_algorithm/baekjoon10952.js
@@ -0,0 +1,24 @@
+const readline = require("readline");
+function solution(input) {
+  let answer = input
+    .map((item) =>
+      item
+        .split(" ")
+        .map((item) => Number(item))
+        .reduce((a, b) => a + b)
+    )
+    .join("\n");
+  return answer;
+}
+let arr = [];
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+rl.on("line", (line) => {
+  arr.push(line);
+  if (line === "0 0") {
+    arr.pop();
+    console.log(solution(arr));
+  }
+});
