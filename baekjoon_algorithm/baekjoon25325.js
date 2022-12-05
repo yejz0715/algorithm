8e6c894184096de896ed2254e459a2de02cba482
unknown <yejz0715@gmail.com>
2022-12-05 21:47:20 +0900

    baekjoon25325_학생 인기도 측정 ing

--- baekjoon_algorithm/baekjoon25325.js
+++ baekjoon_algorithm/baekjoon25325.js
@@ -1,8 +1,23 @@
-//
+//학생 인기도 측정
 const readline = require("readline");
 function solution(input) {
   let nameArr = input[0].split(" ");
-  return nameArr;
+  let answer = "";
+  sortArr = [];
+  for (let j = 0; j < nameArr.length; j++) {
+    let num = 0;
+    let arr = [];
+    for (let i = 1; i < input.length; i++) {
+      arr = input[i].split(" ");
+      for (let k = 0; k < arr.length; k++) {
+        if (nameArr[j] === arr[k]) {
+          num += 1;
+        }
+      }
+    }
+    answer += nameArr[j] + " " + num + "\n";
+  }
+  return answer;
 }
 let arr = [];
 const rl = readline.createInterface({
@@ -11,7 +26,7 @@ const rl = readline.createInterface({
 });
 rl.on("line", (line) => {
   arr.push(line);
-  if (Number(arr[0]) + 1 === arr.length) {
+  if (Number(arr[0]) + 2 === arr.length) {
     arr.shift();
     console.log(solution(arr));
   }
