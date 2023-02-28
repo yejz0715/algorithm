2be1434794ee323a45b14262ef0cbeffbd020747
yejz0715 <yejz0715@gmail.com>
2023-02-28 15:08:19 +0900

    baekjoon10988_팰린드롬 확인하기_reverse()

--- baekjoon_algorithm/baekjoon10988.js
+++ baekjoon_algorithm/baekjoon10988.js
@@ -1,29 +1,8 @@
 //팰린드롬인지 확인하기
 const readline = require("readline");
 function solution(input) {
-  let left = "";
-  let right = "";
-  let rightTmp = "";
-  let answer = 0;
-  let length =
-    input.length % 2 === 0 ? input.length / 2 : Math.floor(input.length / 2);
-
-  for (let i = 0; i < length; i++) {
-    left += input[i];
-  }
-
-  for (let j = input.length - 1; j >= length; j--) {
-    right += input[j];
-  }
-  for (let k = input.length - 1; k > length; k--) {
-    rightTmp += input[k];
-  }
-
-  if (input.length % 2 === 0) {
-    left === right ? (answer = 1) : (answer = 0);
-  } else {
-    left === rightTmp ? (answer = 1) : (answer = 0);
-  }
+  let stringReverse = input.split("").reverse().join("");
+  let answer = input === stringReverse ? 1 : 0;
   return answer;
 }
 const rl = readline.createInterface({
