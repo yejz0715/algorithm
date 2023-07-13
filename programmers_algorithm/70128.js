e018b0a4ac3507f394c335406d2d6b5962daf637
yejz0715 <yejz0715@gmail.com>
2023-07-13 23:43:18 +0900

    70128_내적적

---
+++ programmers_algorithm/70128.js
@@ -0,0 +1,14 @@
+//내적
+function solution(a, b) {
+  let answer = 0;
+  a.map((aItem, aIndex) => {
+    b.map((bItem, bIndex) => {
+      if (aIndex !== bIndex) {
+        return;
+      } else {
+        answer += aItem * bItem;
+      }
+    });
+  });
+  return answer;
+}
