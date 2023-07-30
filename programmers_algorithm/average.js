0d4be156a6f3a458faf7cbf706a55f66de1389b4
yejz0715 <yejz0715@gmail.com>
2023-07-30 23:20:35 +0900

    같은 파일 삭제

--- programmers_algorithm/average.js
+++
@@ -1,7 +0,0 @@
-//average 평균구하기
-function solution(arr) {
-  let agv =
-    arr.map((item) => Number(item)).reduce((a, b) => a + b) / arr.length;
-  return agv;
-}
-console.log(solution([1, 2, 3, 4]));
